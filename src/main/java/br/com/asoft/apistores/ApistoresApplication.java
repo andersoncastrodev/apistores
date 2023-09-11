f6d3425a50c207116cbf337936699148c6fb8588
Anderson Castro <andersoncastrosantos@gmail.com>
2023-09-11 12:49:54 -0300

    inicio

---
+++ src/main/java/br/com/asoft/apistores/ApistoresApplication.java
@@ -0,0 +1,13 @@
+package br.com.asoft.apistores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApistoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApistoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/asoft/apistores/ApistoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.asoft.apistores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApistoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
