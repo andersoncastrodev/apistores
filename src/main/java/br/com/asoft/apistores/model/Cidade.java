fa77dbb98c751a6754a2a6c9786637f2c1ea5793
Anderson Castro <andersoncastrosantos@gmail.com>
2023-09-12 08:48:44 -0300

    adicioando estada, cidade e endereco

---
+++ src/main/java/br/com/asoft/apistores/model/Cidade.java
@@ -0,0 +1,25 @@
+package br.com.asoft.apistores.model;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Cidade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "id_estado")
+    private Estado estado;
+
+}
---
+++ src/main/java/br/com/asoft/apistores/model/Endereco.java
@@ -0,0 +1,27 @@
+package br.com.asoft.apistores.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String rua;
+
+    private String cep;
+
+
+}
