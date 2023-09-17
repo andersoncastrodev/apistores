664352fe5e92fb942629a74d2d1bfc49f10c4da2
Anderson Castro <andersoncastrosantos@gmail.com>
2023-09-17 09:52:37 -0300

    adicionando repository,controller,service de pessoa

--- src/main/java/br/com/asoft/apistores/controller/PessoaController.java
+++ src/main/java/br/com/asoft/apistores/controller/PessoaController.java
@@ -1,9 +1,13 @@
 package br.com.asoft.apistores.controller;
 
+import br.com.asoft.apistores.model.Pessoa;
 import br.com.asoft.apistores.service.PessoaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("pessoas")
@@ -11,5 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class PessoaController {
 
     private final PessoaService pessoaService;
+
+    @GetMapping
+    public List<Pessoa> listaTodas(){
+        return pessoaService.allPessoas();
+    }
 
 }
--- src/main/java/br/com/asoft/apistores/service/PessoaService.java
+++ src/main/java/br/com/asoft/apistores/service/PessoaService.java
@@ -1,9 +1,19 @@
 package br.com.asoft.apistores.service;
 
+import br.com.asoft.apistores.model.Pessoa;
+import br.com.asoft.apistores.respository.PessoaRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class PessoaService {
 
+    private final PessoaRepository pessoaRepository;
 
+    public List<Pessoa> allPessoas(){
+        return pessoaRepository.findAll();
+    }
 }
