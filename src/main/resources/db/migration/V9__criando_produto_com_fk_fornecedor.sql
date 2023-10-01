8d84f5b0d8fbc02558cc193e86ce2e1805851693
Anderson Castro <andersoncastrosantos@gmail.com>
2023-10-01 15:33:25 -0300

    refatoracao

---
+++ src/main/resources/db/migration/V9__criando_produto_com_fk_fornecedor.sql
@@ -0,0 +1,9 @@
+create table produto(
+   id serial not null,
+   descricao varchar(150) not null,
+   valor_compra decimal(10,2),
+   valor_venda decimal(10,2),
+   fornecedor_id int8,
+   constraint produto_pk primary key (id),
+   constraint fk_fornecedor foreign key (fornecedor_id) references fornecedor (id)   
+);
