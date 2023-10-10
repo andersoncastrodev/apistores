dc14a7e059ab81e992db565181c62094395b9194
Anderson Castro <andersoncastrosantos@gmail.com>
2023-10-10 15:30:57 -0300

    adicionando cliente

--- src/main/java/br/com/asoft/apistores/model/Cliente.java
+++ src/main/java/br/com/asoft/apistores/model/Cliente.java
@@ -1,27 +1,25 @@
 package br.com.asoft.apistores.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
+import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Getter
 @Setter
+@Getter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Pessoa {
+public class Cliente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
 
-    private String nome;
+    private String tipo;
 
-    private String telefone;
-
-
+    @OneToOne(optional = false)
+    @JoinColumn(name = "pesssoa_id")
+    private Pessoa pessoa;
 }
