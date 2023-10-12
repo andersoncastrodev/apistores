99c606a11cfcb65dda400aeebb2f680eea22c412
Anderson Castro <andersoncastrosantos@gmail.com>
2023-10-12 12:55:18 -0300

    criando cliente mapper

---
+++ src/main/java/br/com/asoft/apistores/mapper/ClienteMapper.java
@@ -0,0 +1,18 @@
+package br.com.asoft.apistores.mapper;
+
+import br.com.asoft.apistores.inp.ClienteInp;
+import br.com.asoft.apistores.model.Cliente;
+import br.com.asoft.apistores.out.ClienteOut;
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ClienteMapper {
+
+    Cliente toCliente(ClienteInp clienteInp);
+
+    List<ClienteOut> toListClienteOut(List<Cliente> clientes);
+
+}
