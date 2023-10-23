a3e006d6a42158ae4377e601dac38de953f1b58f
Anderson Castro <andersoncastrosantos@gmail.com>
2023-10-23 09:08:49 -0300

    adicionando repositorio de usuario

---
+++ src/main/java/br/com/asoft/apistores/respository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package br.com.asoft.apistores.respository;
+
+import br.com.asoft.apistores.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+}
