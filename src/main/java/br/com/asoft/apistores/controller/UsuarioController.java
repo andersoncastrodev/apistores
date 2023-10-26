ec5ca6e389f04dcaecb731e9099d37ec592a3f6b
Anderson Castro <andersoncastrosantos@gmail.com>
2023-10-26 08:55:40 -0300

    consutla de usuario controller

--- src/main/java/br/com/asoft/apistores/controller/UsuarioController.java
+++ src/main/java/br/com/asoft/apistores/controller/UsuarioController.java
@@ -1,20 +1,27 @@
 package br.com.asoft.apistores.controller;
 
+import br.com.asoft.apistores.mapper.UsuarioMapper;
 import br.com.asoft.apistores.out.UsuarioOut;
 import br.com.asoft.apistores.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("usuarios")
 @RequiredArgsConstructor
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
 
-    public List<UsuarioOut> listarTodos(){
+    private final UsuarioMapper usuarioMapper;
 
+    @GetMapping
+    public List<UsuarioOut> listarUsuarios(){
+        return usuarioMapper.toListUsuarioOut( usuarioService.allTodos());
     }
 
 }
---
+++ src/main/java/br/com/asoft/apistores/mapper/UsuarioMapper.java
@@ -0,0 +1,15 @@
+package br.com.asoft.apistores.mapper;
+
+import br.com.asoft.apistores.model.Usuario;
+import br.com.asoft.apistores.out.UsuarioOut;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface UsuarioMapper {
+
+    UsuarioOut toUsuarioOut(Usuario usuario);
+
+    List<UsuarioOut> toListUsuarioOut(List<Usuario> usuarios);
+}
