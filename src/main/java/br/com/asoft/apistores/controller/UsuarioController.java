35ae0958e6e65478aeb15bda4cb1cc9f474205d5
Anderson Castro <andersoncastrosantos@gmail.com>
2023-10-30 08:44:51 -0300

    TryOuFaill de usuario

--- src/main/java/br/com/asoft/apistores/controller/UsuarioController.java
+++ src/main/java/br/com/asoft/apistores/controller/UsuarioController.java
@@ -5,9 +5,9 @@ import br.com.asoft.apistores.out.UsuarioOut;
 import br.com.asoft.apistores.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
@@ -22,6 +22,12 @@ public class UsuarioController {
     @GetMapping
     public List<UsuarioOut> listarUsuarios(){
         return usuarioMapper.toListUsuarioOut( usuarioService.allTodos());
+    }
+
+    @GetMapping("/{id}")
+    public UsuarioOut buscaId(@PathVariable Long id){
+
+        return null;
     }
 
 }
--- src/main/java/br/com/asoft/apistores/service/UsuarioService.java
+++ src/main/java/br/com/asoft/apistores/service/UsuarioService.java
@@ -1,5 +1,6 @@
 package br.com.asoft.apistores.service;
 
+import br.com.asoft.apistores.exceptions.EntityNotFoundExceptions;
 import br.com.asoft.apistores.model.Usuario;
 
 import br.com.asoft.apistores.respository.UsuarioRepository;
@@ -16,5 +17,15 @@ public class UsuarioService {
 
     public List<Usuario> allTodos(){
         return usuarioRepository.findAll();
+    }
+
+    public Usuario findById(Long id){
+        return tryOrFaill(id);
+    }
+
+
+    public Usuario tryOrFaill(Long id){
+        return usuarioRepository.findById(id)
+                .orElseThrow(()-> new EntityNotFoundExceptions("Usuario",id));
     }
 }
