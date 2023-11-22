02ea37d502d46a0b3003390f17388b1e2bc75cbe
Anderson Castro <andersoncastrosantos@gmail.com>
2023-11-22 07:14:57 -0300

    controller relatorio usuario

--- src/main/java/br/com/asoft/apistores/controller/UsuarioController.java
+++ src/main/java/br/com/asoft/apistores/controller/UsuarioController.java
@@ -7,8 +7,14 @@ import br.com.asoft.apistores.out.UsuarioOut;
 import br.com.asoft.apistores.service.UsuarioService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -50,6 +56,28 @@ public class UsuarioController {
     @DeleteMapping("/{id}")
     public void excluirUsuario(@PathVariable Long id){
           usuarioService.deleteUsuario(id);
+    }
+
+    @GetMapping("/relatoriousuarios")
+    public ResponseEntity<InputStreamResource> relatorioUsuarios(){
+
+        try {
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add("Content-Disposition", "inline; filename=vendas.pdf");
+
+        InputStreamResource relatorio = new InputStreamResource( usuarioService.relatorioUsuarios());
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(relatorio);
+
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
     }
 
 }
