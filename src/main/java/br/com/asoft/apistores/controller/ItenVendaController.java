6a2c28e5cb97482bc0f7204e8028c4b669810cbe
Anderson Castro <andersoncastrosantos@gmail.com>
2023-11-27 08:30:29 -0300

    ajustes

--- src/main/java/br/com/asoft/apistores/controller/ItenVendaController.java
+++ src/main/java/br/com/asoft/apistores/controller/ItenVendaController.java
@@ -1,6 +1,8 @@
 package br.com.asoft.apistores.controller;
 
 import br.com.asoft.apistores.out.ItenVendaOut;
+import br.com.asoft.apistores.service.ItenVendaService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +11,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/itenvendas")
+@RequiredArgsConstructor
 public class ItenVendaController{
 
+    private final ItenVendaService itenVendaService;
     public List<ItenVendaOut> todasItenVendas(){
 
-
+        return null;
     }
 }
