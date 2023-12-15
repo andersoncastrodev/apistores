921e4e8f5b42c529ec85773937ccb4600aa2ca05
Anderson Castro <andersoncastrosantos@gmail.com>
2023-12-15 14:40:24 -0300

    relatorio itemVenda

--- src/main/java/br/com/asoft/apistores/controller/ItemVendaController.java
+++ src/main/java/br/com/asoft/apistores/controller/ItemVendaController.java
@@ -9,8 +9,14 @@ import br.com.asoft.apistores.out.ItemVendaOut;
 import br.com.asoft.apistores.service.ItemVendaService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -53,6 +59,26 @@ public class ItemVendaController {
     public void excluirItemVenda(@PathVariable Long id){
         itemVendaService.deletarItemVenda(id);
     }
+
+
+    @GetMapping("/relatorioitemvendas")
+    public ResponseEntity<InputStreamResource> relatorioPessoas() {
+
+        try {
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Disposition", "inline; filename=pessoas.pdf");
+
+            InputStreamResource relatorio = new InputStreamResource( itemVendaService.relatorioTodasPessoas());
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .contentType(MediaType.APPLICATION_PDF)
+                    .body(relatorio);
+
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
 
 
 
