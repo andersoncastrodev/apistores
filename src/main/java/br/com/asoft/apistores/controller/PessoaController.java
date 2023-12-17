39aa63a96500c71ebe9a864ef9dc5db572853e24
Anderson Castro <andersoncastrosantos@gmail.com>
2023-12-17 18:16:31 -0300

    ajuste em pessoa

--- src/main/java/br/com/asoft/apistores/controller/PessoaController.java
+++ src/main/java/br/com/asoft/apistores/controller/PessoaController.java
@@ -7,14 +7,8 @@ import br.com.asoft.apistores.out.PessoaOut;
 import br.com.asoft.apistores.service.PessoaService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -57,11 +51,6 @@ public class PessoaController {
     @DeleteMapping("/{id}")
     public void excluirPessoa(@PathVariable Long id){
         pessoaService.deletePessoa(id);
-    }
-
-c
-
-
     }
 
 
