af4c4adb97b149d73edaf6c370e6ae1fcee50155
Anderson Castro <andersoncastrosantos@gmail.com>
2023-12-21 16:51:08 -0300

    relatorio de cliente

--- src/main/java/br/com/asoft/apistores/service/ClienteService.java
+++ src/main/java/br/com/asoft/apistores/service/ClienteService.java
@@ -3,10 +3,18 @@ package br.com.asoft.apistores.service;
 import br.com.asoft.apistores.exceptions.EntityNotFoundExceptions;
 import br.com.asoft.apistores.model.Cliente;
 import br.com.asoft.apistores.model.Pessoa;
+import br.com.asoft.apistores.relatorio.Reports;
 import br.com.asoft.apistores.respository.ClienteRepository;
+import com.itextpdf.io.font.constants.StandardFonts;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.property.TextAlignment;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -46,5 +54,37 @@ public class ClienteService {
     public Cliente tryOrFail(Long id){
         return clienteRepository.findById(id)
                 .orElseThrow(()-> new EntityNotFoundExceptions("Cliente",id));
+    }
+
+    public ByteArrayInputStream relatorioCliente() throws IOException {
+
+        Reports reports = new Reports(false);
+
+        reports.addParagraph( new Paragraph("Lista de Cliente")
+                .setMargins(1f,5f,1f,5f)
+                .setFontSize(28)
+                .setTextAlignment(TextAlignment.CENTER)
+                .setFont(PdfFontFactory.createFont(StandardFonts.COURIER_BOLD)));
+
+        reports.addNewLine();
+        reports.openTable(1f,1f,1f,1f);
+        reports.addTableHeader("Codigo Pessoa","Nome","Telefone","Tipo");
+
+        List<Cliente> clientes = findAllCliente();
+
+        for (Cliente cliente: clientes){
+
+            reports.addCellCenter(cliente.getPessoa().getId());
+            reports.addCellCenter(cliente.getPessoa().getNome());
+            reports.addCellCenter(cliente.getPessoa().getTelefone());
+            reports.addCellCenter(cliente.getTipo());
+
+        }
+
+        reports.closeTable();
+        reports.closeDocument();
+
+        return reports.getByteArrayInputStream();
+
     }
 }
