22213759f3df12251dddfefd375f72f1c2c3e36d
Anderson Castro <andersoncastrosantos@gmail.com>
2023-12-29 10:12:30 -0300

    fechado relatori de estado

--- src/main/java/br/com/asoft/apistores/controller/EstadoController.java
+++ src/main/java/br/com/asoft/apistores/controller/EstadoController.java
@@ -9,9 +9,13 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -65,7 +69,22 @@ public class EstadoController {
     @GetMapping("/relatorioestados")
     public ResponseEntity<InputStreamResource> relatorioEstados() {
 
-        return null;
+        try {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "inline; filename=pessoas.pdf");
+
+        InputStreamResource relatorio = new InputStreamResource(estadoService.relatorioEstado());
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(relatorio);
+
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
     }
 
 
