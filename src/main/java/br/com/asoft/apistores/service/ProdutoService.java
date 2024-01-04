9511b61047ec5a087dd2d8eb4ec9e39b69c08b18
Anderson Castro <andersoncastrosantos@gmail.com>
2024-01-04 10:47:24 -0300

    relatorio produtos inicio

--- src/main/java/br/com/asoft/apistores/service/ProdutoService.java
+++ src/main/java/br/com/asoft/apistores/service/ProdutoService.java
@@ -3,10 +3,17 @@ package br.com.asoft.apistores.service;
 import br.com.asoft.apistores.exceptions.EntityNotFoundExceptions;
 import br.com.asoft.apistores.model.Fornecedor;
 import br.com.asoft.apistores.model.Produto;
+import br.com.asoft.apistores.relatorio.Reports;
 import br.com.asoft.apistores.respository.ProdutoRepository;
+import com.itextpdf.io.font.constants.StandardFonts;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.property.TextAlignment;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -47,6 +54,40 @@ public class ProdutoService {
     public Produto tryOrFail(Long id){
         return produtoRepository.findById(id)
                 .orElseThrow(()-> new EntityNotFoundExceptions("Produto",id));
+    }
+
+    public ByteArrayInputStream relatorioTodosProdutos() throws IOException {
+
+        Reports reports = new Reports(false);
+
+        reports.addParagraph( new Paragraph("Lista de Produtos")
+                .setMargins(1f,5f,1f,5f)
+                .setFontSize(28)
+                .setTextAlignment(TextAlignment.CENTER)
+                .setFont(PdfFontFactory.createFont(StandardFonts.COURIER_BOLD)));
+
+        reports.addNewLine();
+
+        reports.openTable(1f,1f,1f,1f,1f);
+
+        reports.addTableHeader("Codigo","Descrição","Valor Comprar","Valor Venda","Fornecedor");
+
+        List<Produto> produtos = allTodos();
+
+        for (Produto produto : produtos ) {
+
+            reports.addCellCenter(produto.getId());
+            reports.addCellCenter(produto.getDescricao());
+            reports.addCellCenter(produto.getValorCompra());
+            reports.addCellCenter(produto.getValorVenda());
+            reports.addCellCenter(produto.getFornecedor().getNome());
+        }
+
+        reports.closeTable();
+        reports.closeDocument();
+
+        return reports.getByteArrayInputStream();
+
     }
 
 
