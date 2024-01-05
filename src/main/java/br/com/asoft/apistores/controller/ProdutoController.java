380e12c0c7d825eedd2e3b7a0d663ed35fbf1f86
Anderson Castro <andersoncastrosantos@gmail.com>
2024-01-05 08:50:09 -0300

    relatorio produto

--- src/main/java/br/com/asoft/apistores/controller/ProdutoController.java
+++ src/main/java/br/com/asoft/apistores/controller/ProdutoController.java
@@ -7,8 +7,14 @@ import br.com.asoft.apistores.out.ProdutoOut;
 import br.com.asoft.apistores.service.ProdutoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -59,6 +65,24 @@ public class ProdutoController {
         produtoService.deletarProduto(id);
     }
 
+    public ResponseEntity<InputStreamResource> relatorioPessoas() {
+
+        try {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "inline; filename=pessoas.pdf");
+
+        InputStreamResource relatorio = new InputStreamResource(produtoService.relatorioTodosProdutos());
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(relatorio);
+
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 
 
 
