010f26d51ad3f7b26cd9ecadb5df18938162d0e4
Anderson Castro <andersoncastrosantos@gmail.com>
2024-01-06 14:27:31 -0300

    ajutes em produto

--- src/main/java/br/com/asoft/apistores/controller/ProdutoController.java
+++ src/main/java/br/com/asoft/apistores/controller/ProdutoController.java
@@ -65,25 +65,25 @@ public class ProdutoController {
         produtoService.deletarProduto(id);
     }
 
-    @GetMapping("/relatorioprodutos")
-    public ResponseEntity<InputStreamResource> relatorioPessoas() {
-
-        try {
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Disposition", "inline; filename=pessoas.pdf");
-
-        InputStreamResource relatorio = new InputStreamResource(produtoService.relatorioTodosProdutos());
-
-        return ResponseEntity.ok()
-                .headers(headers)
-                .contentType(MediaType.APPLICATION_PDF)
-                .body(relatorio);
-
-        } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-    }
+//    @GetMapping("/relatorioprodutos")
+//    public ResponseEntity<InputStreamResource> relatorioPessoas() {
+//
+//        try {
+//
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add("Content-Disposition", "inline; filename=pessoas.pdf");
+//
+//        InputStreamResource relatorio = new InputStreamResource(produtoService.relatorioTodosProdutos());
+//
+//        return ResponseEntity.ok()
+//                .headers(headers)
+//                .contentType(MediaType.APPLICATION_PDF)
+//                .body(relatorio);
+//
+//        } catch (IOException e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+//        }
+//    }
 
 
 
