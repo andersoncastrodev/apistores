2257081a25885f23e28c07feb6239dc01566bd1a
Anderson Castro <andersoncastrosantos@gmail.com>
2024-01-08 19:12:21 -0300

    adicinando produto ao itemVenda

--- src/main/resources/db/migration/V10__criando_item_venda.sql
+++ src/main/resources/db/migration/V10__criando_item_venda.sql
@@ -1,9 +1,11 @@
 create table item_venda(
 	id serial not null,
 	venda_id bigint not null,
+	produto_id bigint not null,
 	quant bigint not null,
 	valor_unid bigint not null,
 	valor_total decimal(10,2) not null,
 	constraint pk_item_venda primary key (id),
+	constraint fk_produto_id foreign key(produto_id) references produto(id),
 	constraint fk_venda_id foreign key (venda_id) references venda(id)
 );
