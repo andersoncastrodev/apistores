2facf0c1a1988ef682baf472d1efb8a3f2a804b4
Anderson Castro <andersoncastrosantos@gmail.com>
2024-01-29 08:18:03 -0300

    adicionado o tyrOrFail

--- src/main/java/br/com/asoft/apistores/service/FormaPagamentoService.java
+++ src/main/java/br/com/asoft/apistores/service/FormaPagamentoService.java
@@ -1,16 +1,32 @@
 package br.com.asoft.apistores.service;
 
+import br.com.asoft.apistores.exceptions.EntityNotFoundExceptions;
+import br.com.asoft.apistores.model.FormaDePagamento;
 import br.com.asoft.apistores.respository.FormaPagamentoRepository;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class FormaPagamentoService {
 
-
     private final FormaPagamentoRepository formaPagamentoRepository;
+
+    public List<FormaDePagamento> findAll(){
+        return formaPagamentoRepository.findAll();
+    }
+
+    public FormaDePagamento findId(Long id){
+        return tryOrFail(id);
+    }
+
+    public FormaDePagamento tryOrFail(Long id){
+        return formaPagamentoRepository.findById(id)
+                .orElseThrow(()-> new EntityNotFoundExceptions("Forma de Pagamento",id));
+    }
+
 
 
 
