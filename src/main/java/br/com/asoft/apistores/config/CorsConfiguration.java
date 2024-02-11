8382b334e84745860efa754748cf9235e557c56e
Anderson Castro <andersoncastrosantos@gmail.com>
2024-02-11 09:59:45 -0300

    Cors classe de configuracao

---
+++ src/main/java/br/com/asoft/apistores/config/CorsConfiguration.java
@@ -0,0 +1,28 @@
+package br.com.asoft.apistores.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+
+                /*
+                   Aqui devemos colocar a url do FrontEnd.
+                   Ex: http://localhost:3000
+                   ou  http://localhots:4200
+                 */
+                //.allowedOrigins("*") Vai permitir tudo.
+            }
+        };
+    }
+}
