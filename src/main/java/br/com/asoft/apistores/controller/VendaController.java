c11e55268c6bbd43ecd226c359c6ae6be3ca13a5
Anderson Castro <andersoncastrosantos@gmail.com>
2024-02-25 16:06:06 -0300

    ajuste em venda

--- src/main/java/br/com/asoft/apistores/controller/VendaController.java
+++ src/main/java/br/com/asoft/apistores/controller/VendaController.java
@@ -10,6 +10,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -34,6 +35,9 @@ public class VendaController {
 
         Page<Venda> vendaPage = vendaService.allTodas(pageable);
 
+        List<VendaOut> vendaOuts = vendaMapper.toListVendaOut(vendaPage.getContent());
+
+    //    Page<VendaOut> vendaOutPage = new PageImpl<>(vendaOutPage,pageable, vendaPage.getTotalElements());
 
      //   return  vendaMapper.toListVendaOut( vendaService.allTodas());
         return null;
