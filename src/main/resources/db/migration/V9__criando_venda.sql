f2287e5e872e7f00d8e726cc3e457fdaed5e9c51
Anderson Castro <andersoncastrosantos@gmail.com>
2024-02-28 09:23:48 -0300

    update no flyway

--- src/main/resources/db/migration/V9__criando_venda.sql
+++ src/main/resources/db/migration/V9__criando_venda.sql
@@ -3,6 +3,7 @@ create table venda(
    usuario_cod bigint not null,
    cliente_cod bigint not null,
    valor_total decimal(10,2) not null,
+   tipo_pagamento bigint,
    constraint pk_venda primary key (id),
    constraint fk_usuario foreign key (usuario_cod) references usuario(id),
    constraint fk_cliente foreign key (cliente_cod) references cliente(id)
