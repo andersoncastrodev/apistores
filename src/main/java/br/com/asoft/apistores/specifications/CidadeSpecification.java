77afc229a093d73f1d8f096fa2176db22d05c2a8
Anderson Castro <andersoncastrosantos@gmail.com>
2024-04-19 11:50:17 -0300

    adicinando o predicate Cidade

--- src/main/java/br/com/asoft/apistores/specifications/CidadeSpecification.java
+++ src/main/java/br/com/asoft/apistores/specifications/CidadeSpecification.java
@@ -2,7 +2,10 @@ package br.com.asoft.apistores.specifications;
 
 import br.com.asoft.apistores.filter.CidadeFilter;
 import br.com.asoft.apistores.model.Cidade;
+import jakarta.persistence.criteria.Predicate;
 import org.springframework.data.jpa.domain.Specification;
+
+import java.util.ArrayList;
 
 public class CidadeSpecification {
 
@@ -10,11 +13,13 @@ public class CidadeSpecification {
         return (root, query, criteriaBuilder) -> {
 
 
+            var predicates = new ArrayList<>();
 
+            if(cidadeFilter.getNome() != null ) {
+                predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("nome")),"%"+cidadeFilter.getNome()+"%"));
+            }
 
-
-
-            return null;
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         };
     }
 }
