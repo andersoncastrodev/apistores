f176fa775d4b2e4e7be54e53baae22f0e6d0ce92
Anderson Castro <andersoncastrosantos@gmail.com>
2024-04-25 08:51:46 -0300

    inicio specification cliente

---
+++ src/main/java/br/com/asoft/apistores/specifications/ClienteSpecification.java
@@ -0,0 +1,11 @@
+package br.com.asoft.apistores.specifications;
+
+import br.com.asoft.apistores.model.Cliente;
+import org.springframework.data.jpa.domain.Specification;
+
+public class ClienteSpecification {
+
+    public static Specification<Cliente> filter(ClienteFilter clienteFilter) {
+
+    }
+}
