d73190e77a27be6b10b3d02b49a2fd88993b962a
Anderson Castro <andersoncastrosantos@gmail.com>
2024-04-27 10:06:47 -0300

    continuando Specification cliente

--- src/main/java/br/com/asoft/apistores/respository/ClienteRepository.java
+++ src/main/java/br/com/asoft/apistores/respository/ClienteRepository.java
@@ -2,8 +2,9 @@ package br.com.asoft.apistores.respository;
 
 import br.com.asoft.apistores.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente,Long> {
+public interface ClienteRepository extends JpaRepository<Cliente,Long> , JpaSpecificationExecutor<Cliente> {
 }
--- src/main/java/br/com/asoft/apistores/specifications/ClienteSpecification.java
+++ src/main/java/br/com/asoft/apistores/specifications/ClienteSpecification.java
@@ -1,11 +1,28 @@
 package br.com.asoft.apistores.specifications;
 
+import br.com.asoft.apistores.filter.ClienteFilter;
 import br.com.asoft.apistores.model.Cliente;
+import jakarta.persistence.criteria.Predicate;
 import org.springframework.data.jpa.domain.Specification;
+
+import java.util.ArrayList;
 
 public class ClienteSpecification {
 
     public static Specification<Cliente> filter(ClienteFilter clienteFilter) {
+        return (root, query, criteriaBuilder) -> {
 
+            var preditaces = new ArrayList<>();
+
+            // Fazendo a junção com a entidade Pessoa
+            root.join("pessoa");
+
+            // Adicionando o critério de Like para o nome da pessoa
+            if (clienteFilter.getPessoa().getNome() != null) {
+                preditaces.add(criteriaBuilder.like(root.get("pessoa").get(("nome")), "%"+clienteFilter.getPessoa().getNome()+"%"));
+            }
+
+            return criteriaBuilder.and(preditaces.toArray(new Predicate[0]));
+        };
     }
 }
