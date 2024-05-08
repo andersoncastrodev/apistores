42e29e07b293f686940d4f4942c01f670d79697f
Anderson Castro <andersoncastrosantos@gmail.com>
2024-05-08 10:42:15 -0300

    .

--- src/main/java/br/com/asoft/apistores/specifications/EnderecoSpecification.java
+++ src/main/java/br/com/asoft/apistores/specifications/EnderecoSpecification.java
@@ -2,15 +2,23 @@ package br.com.asoft.apistores.specifications;
 
 import br.com.asoft.apistores.filter.EnderecoFilter;
 import br.com.asoft.apistores.model.Endereco;
+import jakarta.persistence.criteria.Predicate;
 import org.springframework.data.jpa.domain.Specification;
+import java.util.ArrayList;
 
 public class EnderecoSpecification {
 
     public static Specification<Endereco> filter(EnderecoFilter enderecoFilter) {
         return (root, query, criteriaBuilder) -> {
 
+            var predicates = new ArrayList<Predicate>();
 
-          return null;
+            if (enderecoFilter.getId() != null) {
+                predicates.add(criteriaBuilder.equal(root.get("id"), enderecoFilter.getId()));
+            }
+
+          return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+
         };
     }
 }
