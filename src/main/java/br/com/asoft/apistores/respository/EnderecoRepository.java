15b4139f72b4f946523bd158c2595f430cb3e736
Anderson Castro <andersoncastrosantos@gmail.com>
2024-05-09 12:51:41 -0300

    alterar enderecoRepository

--- src/main/java/br/com/asoft/apistores/respository/EnderecoRepository.java
+++ src/main/java/br/com/asoft/apistores/respository/EnderecoRepository.java
@@ -2,8 +2,9 @@ package br.com.asoft.apistores.respository;
 
 import br.com.asoft.apistores.model.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+public interface EnderecoRepository extends JpaRepository<Endereco, Long>, JpaSpecificationExecutor<Endereco> {
 }
