084e70e10c457797635baba51c1f1befda8f8f11
Anderson Castro <andersoncastrosantos@gmail.com>
2024-05-23 08:24:03 -0300

    .

---
+++ src/main/java/br/com/asoft/apistores/specifications/EstadoSpecification.java
@@ -0,0 +1,11 @@
+package br.com.asoft.apistores.specifications;
+
+import br.com.asoft.apistores.model.Estado;
+import org.springframework.data.jpa.domain.Specification;
+
+public class EstadoSpecification {
+
+    public static Specification<Estado> filter() {
+        return null;
+    }
+}
