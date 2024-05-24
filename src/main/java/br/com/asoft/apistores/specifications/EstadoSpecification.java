df81728be497147d83218442e31eb277ce2fa9b8
Anderson Castro <andersoncastrosantos@gmail.com>
2024-05-24 14:53:25 -0300

    add specifiction

--- src/main/java/br/com/asoft/apistores/specifications/EstadoSpecification.java
+++ src/main/java/br/com/asoft/apistores/specifications/EstadoSpecification.java
@@ -1,11 +1,22 @@
 package br.com.asoft.apistores.specifications;
 
+import br.com.asoft.apistores.filter.EstadoFilter;
 import br.com.asoft.apistores.model.Estado;
 import org.springframework.data.jpa.domain.Specification;
 
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
 public class EstadoSpecification {
 
-    public static Specification<Estado> filter() {
-        return null;
+    public static Specification<Estado> filter(EstadoFilter estadoFilter) {
+        return (root, query, criteriaBuilder) -> {
+
+            var predicates = new ArrayList<>();
+
+
+
+            return null;
+        };
     }
 }
