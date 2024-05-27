4c7a8a1ce1a6f24dd633369b374df78c22ea2ab9
Anderson Castro <andersoncastrosantos@gmail.com>
2024-05-27 08:27:01 -0300

    add spec repository

--- src/main/java/br/com/asoft/apistores/respository/EstadoRepository.java
+++ src/main/java/br/com/asoft/apistores/respository/EstadoRepository.java
@@ -2,8 +2,9 @@ package br.com.asoft.apistores.respository;
 
 import br.com.asoft.apistores.model.Estado;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EstadoRepository extends JpaRepository<Estado,Long> {
+public interface EstadoRepository extends JpaRepository<Estado,Long>, JpaSpecificationExecutor<Estado> {
 }
