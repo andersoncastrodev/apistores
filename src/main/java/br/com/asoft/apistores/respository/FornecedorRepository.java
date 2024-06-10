338fa55b70e56282ef5527f09cfa40cc9c04d386
Anderson Castro <andersoncastrosantos@gmail.com>
2024-06-10 07:40:12 -0300

    adicionando o JpaSpecificationExecutor

--- src/main/java/br/com/asoft/apistores/respository/FornecedorRepository.java
+++ src/main/java/br/com/asoft/apistores/respository/FornecedorRepository.java
@@ -2,8 +2,10 @@ package br.com.asoft.apistores.respository;
 
 import br.com.asoft.apistores.model.Fornecedor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FornecedorRepository extends JpaRepository<Fornecedor,Long> {
+public interface FornecedorRepository extends JpaRepository<Fornecedor,Long>
+        , JpaSpecificationExecutor<Fornecedor> {
 }
