9a8f8d3501e02afc4ab5e4e5feaa1a04ab03f8dc
Anderson Castro <andersoncastrosantos@gmail.com>
2024-06-20 07:35:43 -0300

    ajuste no controller

--- src/main/java/br/com/asoft/apistores/controller/FormaPagamentoController.java
+++ src/main/java/br/com/asoft/apistores/controller/FormaPagamentoController.java
@@ -1,8 +1,10 @@
 package br.com.asoft.apistores.controller;
 
+import br.com.asoft.apistores.mapper.FormaPagamentoMapper;
 import br.com.asoft.apistores.out.FormaPagamentoOut;
 import br.com.asoft.apistores.service.FormaPagamentoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +17,13 @@ public class FormaPagamentoController {
 
     private final FormaPagamentoService formaPagamentoService;
 
+    private final FormaPagamentoMapper formaDePagamentoMapper;
 
+
+    @GetMapping
    public List<FormaPagamentoOut> findAllFormagamento(){
 
-       formaPagamentoService.findAll();
+       List<FormaPagamentoOut>  formaPagamentoOuts ;
 
        return  null;
    }
