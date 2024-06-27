aab54c10b8ecd5d212429f13a85f1e874359e355
Anderson Castro <andersoncastrosantos@gmail.com>
2024-06-27 10:10:15 -0300

    specification de formaPagamento

--- src/main/java/br/com/asoft/apistores/respository/FormaPagamentoRepository.java
+++ src/main/java/br/com/asoft/apistores/respository/FormaPagamentoRepository.java
@@ -2,8 +2,9 @@ package br.com.asoft.apistores.respository;
 
 import br.com.asoft.apistores.model.FormaDePagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FormaPagamentoRepository extends JpaRepository<FormaDePagamento,Long> {
+public interface FormaPagamentoRepository extends JpaRepository<FormaDePagamento,Long> , JpaSpecificationExecutor<FormaDePagamento> {
 }
--- src/main/java/br/com/asoft/apistores/service/FormaPagamentoService.java
+++ src/main/java/br/com/asoft/apistores/service/FormaPagamentoService.java
@@ -1,9 +1,13 @@
 package br.com.asoft.apistores.service;
 
 import br.com.asoft.apistores.exceptions.EntityNotFoundExceptions;
+import br.com.asoft.apistores.filter.FormaPagamentoFilter;
 import br.com.asoft.apistores.model.FormaDePagamento;
 import br.com.asoft.apistores.respository.FormaPagamentoRepository;
+import br.com.asoft.apistores.specifications.FormaPagamentoSpecification;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,8 +18,9 @@ public class FormaPagamentoService {
 
     private final FormaPagamentoRepository formaPagamentoRepository;
 
-    public List<FormaDePagamento> findAll(){
-        return formaPagamentoRepository.findAll();
+    public Page<FormaDePagamento> findAll(FormaPagamentoFilter formaPagamentoFilter, Pageable pageable){
+
+        return formaPagamentoRepository.findAll(FormaPagamentoSpecification.filter(formaPagamentoFilter), pageable);
     }
 
     public FormaDePagamento findId(Long id){
