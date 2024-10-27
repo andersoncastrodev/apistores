ea15212e362cc7795ec29ed00f07600db89d17a2
Anderson Castro <andersoncastrosantos@gmail.com>
2024-10-27 13:46:43 -0300

    .

--- src/main/java/br/com/asoft/apistores/controller/EnderecoController.java
+++ src/main/java/br/com/asoft/apistores/controller/EnderecoController.java
@@ -47,10 +47,10 @@ public class EnderecoController {
 //        return enderecos;
 //    }
 //
-//    @GetMapping("/{id}")
-//    public EnderecoOut buscaPorId(@PathVariable Long id) {
-//        return enderecoMapper.toEnderecoOut(enderecoService.findId(id));
-//    }
+    @GetMapping("/{id}")
+    public EnderecoOut buscaPorId(@PathVariable Long id) {
+        return enderecoMapper.toEnderecoOut(enderecoService.findId(id));
+    }
 
     @PostMapping
     public EnderecoOut salvarEndereco(@RequestBody @Valid EnderecoInp enderecoInp) {
