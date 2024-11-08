05a0f8dd6777bc115af677c2220b58ec95539503
Anderson Castro <andersoncastrosantos@gmail.com>
2024-11-08 06:37:08 -0300

    .

--- src/main/java/br/com/asoft/apistores/controller/ClienteController.java
+++ src/main/java/br/com/asoft/apistores/controller/ClienteController.java
@@ -45,10 +45,10 @@ public class ClienteController {
 
 // SEM PAGINACAO
 //    @GetMapping
-   public List<ClienteOut> todosClientes(){
-        return clienteMapper.toListClienteOut(clienteService.findAllCliente());
-    }
-    }
+//   public List<ClienteOut> todosClientes(){
+//        return clienteMapper.toListClienteOut(clienteService.findAllCliente());
+//    }
+//    }
 
     @GetMapping("/{id}")
     public ClienteOut buscarPorId(@PathVariable Long id){
