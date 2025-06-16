6b20d9dff1478f8bf0d167bb259f1c2c47f11480
Anderson Castro <andersoncastrosantos@gmail.com>
2025-06-16 08:35:08 -0300

    ajuste nos imports

--- src/main/java/br/com/asoft/apistores/controller/SalesController.java
+++ src/main/java/br/com/asoft/apistores/controller/SalesController.java
@@ -8,16 +8,11 @@ import br.com.asoft.apistores.out.VendaOut;
 import br.com.asoft.apistores.service.SalesService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.io.IOException;
+
 import java.util.List;
 
 @RestController
