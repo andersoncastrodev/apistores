44700e7edbf359e51450a052dde580f372555c59
Anderson Castro <andersoncastrosantos@gmail.com>
2025-06-26 08:10:52 -0300

    ajuste na interface

--- src/main/java/br/com/asoft/apistores/respository/UsersRepository.java
+++ src/main/java/br/com/asoft/apistores/respository/UsersRepository.java
@@ -1,10 +1,8 @@
 package br.com.asoft.apistores.respository;
 
 import br.com.asoft.apistores.model.Users;
-import org.apache.poi.sl.draw.geom.GuideIf;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
